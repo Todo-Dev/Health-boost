d432b9e7e7c6916643819b7f97fc29411e632187
SufianHamdan <sufian_hamdan@yahoo.com>
2021-09-05 18:30:26 +0300

    changes

--- app/src/main/java/health/boost/MainActivity.java
+++ app/src/main/java/health/boost/MainActivity.java
@@ -3,11 +3,13 @@ package health.boost;
 import  androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+
+          }
 }
