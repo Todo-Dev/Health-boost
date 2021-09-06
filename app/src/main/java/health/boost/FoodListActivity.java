8c1f82393f4749595253fdae78e1798da128e754
Bayan Khalil <bayan.khalil92@yahoo.com>
2021-09-06 09:24:15 +0300

    api

---
+++ app/src/main/java/health/boost/FoodListActivity.java
@@ -0,0 +1,16 @@
+package health.boost;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FoodListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_food_list);
+
+
+    }
+}
--- app/src/main/java/health/boost/MainActivity.java
+++ app/src/main/java/health/boost/MainActivity.java
@@ -1,7 +1,35 @@
 package health.boost;
 
-import  androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.app.VoiceInteractor;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.StringReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -10,6 +38,58 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView textView = findViewById(R.id.textView);
+        Button button = findViewById(R.id.getData);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                RequestQueue requestQueue = Volley.newRequestQueue(MainActivity.this);
+                String url = "https://api.spoonacular.com/recipes/findByNutrients?minCarbs=10&maxCarbs=50&number=50&apiKey=0af706a2f4d74bfc8f4d0c9aaf2625d4";
+                JsonArrayRequest request=new JsonArrayRequest(Request.Method.GET,url,null ,new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        String title="";
+                        try {
+                            JSONObject vb=response.getJSONObject(0);
+                            for(int i = 0; i < vb.length(); i++) {
+                                title = vb.getString("title");
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
 
-          }
+                        Toast.makeText(MainActivity.this, "title" + title, Toast.LENGTH_LONG).show();
+
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                    Toast.makeText(MainActivity.this, "Error", Toast.LENGTH_LONG).show();
+
+                    }
+                });
+//                StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+//                        new Response.Listener<String>() {
+//                            @Override
+//                            public void onResponse(String response) {
+//                                Toast.makeText(MainActivity.this, response, Toast.LENGTH_LONG).show();
+//                            }
+//                        }, new Response.ErrorListener() {
+//                    @Override
+//                    public void onErrorResponse(VolleyError error) {
+//                        Toast.makeText(MainActivity.this, "Error", Toast.LENGTH_SHORT).show();
+//                    }
+//
+//
+//                });
+                requestQueue.add(request);
+
+
+            }
+        });
+
+    }
+
+
 }
