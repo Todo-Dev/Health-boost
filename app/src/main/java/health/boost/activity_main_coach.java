26ad4b6a1c3315175f89d1a7052ce588bf26eec5
mmohiesen96 <mmohiesen996@gmail.com>
2021-09-06 20:41:41 +0300

    Added two coach and student activities and linked with the login

---
+++ app/src/main/java/health/boost/activity_main_coach.java
@@ -0,0 +1,14 @@
+package health.boost;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_main_coach extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_coach);
+    }
+}
