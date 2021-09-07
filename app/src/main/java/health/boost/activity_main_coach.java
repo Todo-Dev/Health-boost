c00acffdcb2b6642ed90dacd6554a987d96d1d3e
Bayan Khalil <bayan.khalil92@yahoo.com>
2021-09-08 02:47:22 +0300

    video

--- app/src/main/java/health/boost/activity_main_coach.java
+++ app/src/main/java/health/boost/activity_main_coach.java
@@ -3,9 +3,13 @@ package health.boost;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 public class activity_main_coach extends AppCompatActivity {
 
@@ -13,6 +17,9 @@ public class activity_main_coach extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_coach);
+
+
+
 
         Button studentActivity = findViewById(R.id.gotostudentactivity);
         studentActivity.setOnClickListener(new View.OnClickListener() {
@@ -31,5 +38,17 @@ public class activity_main_coach extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+
+        VideoView videoView =findViewById(R.id.videoView);
+        videoView.setVideoPath("android.resource://" + getPackageName() + "/" + R.raw.green);
+
+        MediaController mediaController = new MediaController(this);
+        //link mediaController to videoView
+        mediaController.setAnchorView(videoView);
+        //allow mediaController to control our videoView
+        videoView.setMediaController(mediaController);
+        videoView.start();
+
     }
 }
