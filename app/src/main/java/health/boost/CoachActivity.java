4adb83c2aec0f030b221bc93543a44c6152c7f2c
morjaradat <morjaradat66@gmail.com>
2021-09-07 15:21:41 +0300

    crate bottom navbar for student and coach and create for each navbar their fragments

---
+++ app/src/main/java/health/boost/CoachActivity.java
@@ -0,0 +1,39 @@
+package health.boost;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import health.boost.databinding.ActivityCoachBinding;
+
+
+public class CoachActivity extends AppCompatActivity {
+
+    private static final String TAG = "CoachActivity";
+    private ActivityCoachBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityCoachBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        BottomNavigationView navView = findViewById(R.id.nav_view_coach);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_home_coach, R.id.navigation_recipe_coach, R.id.navigation_Setting_coach)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_coach);
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(binding.navViewCoach, navController);
+
+    }
+}
